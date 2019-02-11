e646a5d8564e9f510f5244ed7222e2ef727ef671
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-11 18:30:55 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+return []
+end
+
+def array_with_two_elements
+  return [1,2]
+end
+
+def first_element(elements)
+  return elements[0]
+end
+
+def third_element(elements)
+  return elements[2]
+end
+
+def last_element(elements)
+  return elements[-1]
+end
+
+def first_element_with_array_methods(elements)
+  return elements.first
+end
+
+def last_element_with_array_methods(elements)
+  return elements.last
+end
+
+def length_of_array(elements)
+  return elements.length
+end
